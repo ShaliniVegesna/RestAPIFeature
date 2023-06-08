0b66f9380924ad22665ef10b6c03e932f934b269
ShaliniVegesna <shalini.vegesna@gmail.com>
2023-06-08 15:10:28 +0530

    Commit to feature branch

--- src/main/java/com/UserDemo/SpringBootMain.java
+++ src/main/java/com/UserDemo/SpringBootMain.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootMain {
 
 	public static void main(String[] args) {
-		System.out.println("Main");
+		System.out.println("Main test Feature Branch");
 		SpringApplication.run(SpringBootMain.class, args);
 	}
 
